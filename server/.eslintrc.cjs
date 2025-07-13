3502c0acd51fad89062417e9ce2b034965d99fa5
Harjot Singh <harjotsingh01425@gmail.com>
2025-07-13 19:32:47 +0530

    Initial commit

---
+++ server/.eslintrc.cjs
@@ -0,0 +1,20 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    semi: ['error', 'always'],
+    quotes: ['error', 'double'],
+    '@typescript-eslint/no-unused-vars': ['error'],
+  },
+  ignorePatterns: ['dist/', 'node_modules/'],
+};
