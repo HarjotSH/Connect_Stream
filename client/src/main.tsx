36f28c236db755dfbd3e2a15e49ad837a9a96d85
Harjot Singh <harjotsingh01425@gmail.com>
2025-12-29 19:05:29 +0530

    corrected path error

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import App from './App'
 import './index.css'
-import { SocketProvider } from './Context/SocketContext.tsx'
+import { SocketProvider } from './Context/SocketContext'
 import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
